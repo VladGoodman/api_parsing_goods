43c85b490aae98cc3abc2fc297fb6d5ab8428018
Владислав Черепушкин <vlad.cher2001@mail.ru>
2021-09-12 19:15:38 +0700

    Added restart requests

--- api_connect.php
+++ api_connect.php
@@ -1,16 +1,17 @@
 <?php
 
+
 namespace App;
 
 class Connect
 {
     public $url;
-    private $path;
     private $method;
     public $result_url;
     public $token;
     public $start;
     public $end;
+    public $sleep = 5;
 
     public function __construct($url, $method, $start, $end)
     {
@@ -30,7 +31,7 @@ class Connect
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => "",
             CURLOPT_MAXREDIRS => 5,
-            CURLOPT_TIMEOUT => 0,
+            CURLOPT_TIMEOUT => 60,
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $this->method,
@@ -43,8 +44,26 @@ class Connect
 
         $response = curl_exec($curl);
         $getInfo = curl_getinfo($curl);
-
         curl_close($curl);
-        return $response;
+
+        if($getInfo['http_code'] !== 200 ){
+            if($getInfo['http_code'] === 202){
+                print_r("\n[".date('H:i:s')."] Апи долго не отвечает, перезапуск через $this->sleep секунд...\n");
+                sleep($this->sleep);
+                $this->getInfoForApi();
+            }
+            if($getInfo['http_code'] === 401){
+                exit("\n[".date('H:i:s')."] Токен в файле token.txt не является действительным\n");
+            }
+            if($getInfo['http_code'] === 429){
+                exit("\n[".date('H:i:s')."] Допустимое колличество запросов закончилось\n");
+            } else{
+                print_r("\n[".date('H:i:s')."] Апи долго не отвечает, перезапуск через $this->sleep секунд...\n");
+                sleep($this->sleep);
+                $this->getInfoForApi();
+            }
+        }else{
+            return $response;
+        }
     }
 }
